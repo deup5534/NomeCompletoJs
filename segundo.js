6cc9ac783c186bf269d19fc9c4eb94c2161aa18a
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2023-04-10 22:37:27 -0300

    quase

--- segundo.js
+++ segundo.js
@@ -1,4 +1,4 @@
 let nome = prompt("Digite seu nome:")
 let sobrenome = prompt("Agora seu sobrenome:")
-alert(`Seu nome é, ${nome}, ${sobrenome}`)
+alert(`Seu nome é, ${nome.charAt(1)}, ${sobrenome}`)
 alert(`${sobrenome}, ${nome}`)
