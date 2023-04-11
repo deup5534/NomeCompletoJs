0a6a52346be8b8a39b02e48e2856b3e40feb8996
Thiago Bezerra de Carvalho <112219943+thiaguinhodopagode@users.noreply.github.com>
2023-04-11 19:34:27 -0300

    Quase

---
+++ index.js
@@ -0,0 +1,5 @@
+let Nome = prompt("Digite o nome:")
+let SobreNome = prompt("Agora seu sobrenome:")
+
+alert(`Seu nome é, ${Nome} ${SobreNome}`)
+alert(`E o de catálogo é, ${SobreNome.toUpperCase()}, ${Nome}`) 
